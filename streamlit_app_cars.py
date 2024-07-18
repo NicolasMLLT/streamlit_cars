4b8cb17dcb5840bccde5bed9d00af99d50984d02
Nicolas Maillet <nicolasmaillet90@gmail.com>
2024-07-18 15:01:59 +0200

    Update streamlit_app_cars.py

--- streamlit_app_cars.py
+++ streamlit_app_cars.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df_cars = pd.read_csv(link)
+data = pd.read_csv(link)
 
 # Here we use "magic commands":
-df_cars
+data
 
 def filter_by_region(data, region):
     return data[data['continent'] == region]
